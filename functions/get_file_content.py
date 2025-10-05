b3fe92fa36559a2be84b8b47b7ed497470bdc2b6
horstrs <horst.marcelo@gmail.com>
2025-10-05 16:48:52 -0300

    truncation treatment changed

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -8,14 +8,13 @@ def get_file_content(working_directory, file_path):
 
         with open(file_path, "r") as f:
             file_content_string = f.read(MAX_CHARS)
-            if len(file_content_string) == MAX_CHARS:
+            if os.path.getsize(file_path) > MAX_CHARS:
                 file_content_string += f"[...File '{file_path}' truncated at 10000 characters]"
         
         return file_content_string
     except Exception as error:
         return f"Error: {error}"
 
-    
 
 def get_file_path(working_directory, file):
 
